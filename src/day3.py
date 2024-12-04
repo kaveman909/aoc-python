bd1e6e73a66d1bab1f6fe636e96f9dda9adc7116
David Tracy <david.tracy3@gmail.com>
2024-12-03 21:50:07 -0700

    optimization for day3

--- src/day3.py
+++ src/day3.py
@@ -2,31 +2,25 @@
 
 import re
 import sys
+import functools
 
-lines = open(sys.argv[1]).readlines()
+lines = functools.reduce(lambda x, y: x.strip() + y, open(sys.argv[1]).readlines(), "")
 
-total = 0
-for row in lines:
-  matches = re.findall(r'mul\((\d+),(\d+)\)', row)
-  total += sum([int(m[0]) * int(m[1]) for m in matches])
+matches = re.findall(r'mul\((\d+),(\d+)\)', lines)
+total = sum([int(m[0]) * int(m[1]) for m in matches])
 
 print(f"Part 1: {total}")
 
 total = 0
 on = True
-for row in lines:
-  muls = list(re.finditer(r'mul\((\d+),(\d+)\)', row))
-  muls_is = [x.span()[0] for x in muls]
-  dos = [x.span()[0] for x in re.finditer(r'do\(\)', row)]
-  donts = [x.span()[0] for x in re.finditer(r"don't\(\)", row)]
-  
-  for i in range(len(row)):
-    if i in dos:
-      on = True
-    elif i in donts:
-      on = False
-    elif (i in muls_is) and on:
-      idx = muls_is.index(i)
-      total += int(muls[idx].group(1)) * int(muls[idx].group(2))
+
+matches = re.findall(r'(do\(\)|don\'t\(\))|mul\((\d+),(\d+)\)', lines)
+for match in matches:
+  if match[0] == "do()":
+    on = True
+  elif match[0] == "don't()":
+    on = False
+  elif on:
+    total += int(match[1]) * int(match[2])
 
 print(f"Part 2: {total}")
