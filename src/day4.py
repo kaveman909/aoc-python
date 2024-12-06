403f2d295dd01cf71757fbdb63db3bb46c209409
David Tracy <david.tracy3@gmail.com>
2024-12-05 20:49:52 -0700

    day 4 pt 2

--- src/day4.py
+++ src/day4.py
@@ -9,6 +9,12 @@ XMAS = "XMAS"
 total = 0
 lines = np.array([[c for c in line.strip()] for line in open(sys.argv[1]).readlines()])
 
+def nbox(lines, start, xbox):
+  if (start + len(xbox)) <= len(lines):
+    return lines[start:start + len(xbox)]
+  else:
+    return None
+
 def process_line(line):
   s = "".join(line)
   return len(re.findall(XMAS, s))
@@ -21,4 +27,21 @@ for i in range(4):
     total += process_line(line)
   lines = np.rot90(lines)
 
-print(total)
+print(f"Part 1: {total}")
+
+xbox = np.array([["M", ".", "M"],
+                 [".", "A", "."],
+                 ["S", ".", "S"]])
+
+inter = 0
+for _ in range(4):
+  i = 0
+  while (nline := nbox(lines, i, xbox)) is not None:
+    matches = []
+    for x, n in zip(xbox, nline):
+      matches.append([m.span()[0] for m in re.finditer(f"(?=({''.join(x)}))", "".join(n))])
+    inter += len(set(matches[0]) & set(matches[1]) & set(matches[2]))
+    i += 1
+  xbox = np.rot90(xbox)
+
+print(f"Part 2: {inter}")
