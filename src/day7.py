31d89b417c2a401d0cfb9bcd72e6a267ed97d8ef
David Tracy <david.tracy3@gmail.com>
2024-12-07 17:56:02 -0700

    day 7 pt 2

--- src/day7.py
+++ src/day7.py
@@ -15,25 +15,33 @@ def mul(x, y):
   return x * y
 
 
-ops = [add, mul]
+def concat(x, y):
+  return int(str(x) + str(y))
 
-total = 0
 
-for line in lines:
-  result = line[0]
-  args = line[1:]
-  op_combos = list(itertools.product(ops, repeat=len(args) - 1))
-  # [(<function add at 0x1008d89a0>,), (<function mul at 0x1008d94e0>,)]
-  for op_combo in op_combos:
-    # (<function add at 0x1008d89a0>,)
-    for i in range(len(op_combo)):
-      # [10, 19]
-      if i == 0:
-        r = op_combo[i](args[i], args[i + 1])
-      else:
-        r = op_combo[i](r, args[i + 1])
-    if r == result:
-      total += result
-      break
+ops_1 = [add, mul]
+ops_2 = [add, mul, concat]
 
-print(f"Part 1: {total}")
+
+def calibration_results(ops):
+  total = 0
+  for line in lines:
+    result = line[0]
+    args = line[1:]
+    op_combos = list(itertools.product(ops, repeat=len(args) - 1))
+    # [(<function add at 0x1008d89a0>,), (<function mul at 0x1008d94e0>,)]
+    for op_combo in op_combos:
+      # (<function add at 0x1008d89a0>,)
+      for i in range(len(op_combo)):
+        # [10, 19]
+        if i == 0:
+          r = op_combo[i](args[i], args[i + 1])
+        else:
+          r = op_combo[i](r, args[i + 1])
+      if r == result:
+        total += result
+        break
+  return total
+
+print(f"Part 1: {calibration_results(ops_1)}")
+print(f"Part 2: {calibration_results(ops_2)}")
