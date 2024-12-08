3cfc6aa80ffcff7a35310c3ee98bf4d97491a7c8
David Tracy <david.tracy3@gmail.com>
2024-12-07 18:07:14 -0700

    day 7 pt 2 with multiproc

--- src/day7.py
+++ src/day7.py
@@ -3,6 +3,8 @@
 import sys
 import re
 import itertools
+from multiprocessing import Pool, cpu_count
+from functools import partial
 
 lines = [list(map(int, re.split(r"[\s:]+", l.strip()))) for l in open(sys.argv[1]).readlines()]
 
@@ -23,25 +25,31 @@ ops_1 = [add, mul]
 ops_2 = [add, mul, concat]
 
 
-def calibration_results(ops):
-  total = 0
-  for line in lines:
-    result = line[0]
-    args = line[1:]
-    op_combos = list(itertools.product(ops, repeat=len(args) - 1))
-    # [(<function add at 0x1008d89a0>,), (<function mul at 0x1008d94e0>,)]
-    for op_combo in op_combos:
-      # (<function add at 0x1008d89a0>,)
-      for i in range(len(op_combo)):
-        # [10, 19]
-        if i == 0:
-          r = op_combo[i](args[i], args[i + 1])
-        else:
-          r = op_combo[i](r, args[i + 1])
-      if r == result:
-        total += result
-        break
-  return total
+def calibrate(line, ops):
+  result = line[0]
+  args = line[1:]
+  op_combos = list(itertools.product(ops, repeat=len(args) - 1))
+  # [(<function add at 0x1008d89a0>,), (<function mul at 0x1008d94e0>,)]
+  for op_combo in op_combos:
+    # (<function add at 0x1008d89a0>,)
+    for i in range(len(op_combo)):
+      # [10, 19]
+      if i == 0:
+        r = op_combo[i](args[i], args[i + 1])
+      else:
+        r = op_combo[i](r, args[i + 1])
+    if r == result:
+      return result
+  return 0
 
-print(f"Part 1: {calibration_results(ops_1)}")
-print(f"Part 2: {calibration_results(ops_2)}")
+
+def calibration_results(ops):
+  calibrate_with_ops = partial(calibrate, ops=ops)
+  with Pool(cpu_count()) as pool:
+    results = pool.map(calibrate_with_ops, lines)
+  return sum(results)
+
+
+if __name__ == "__main__":
+  print(f"Part 1: {calibration_results(ops_1)}")
+  print(f"Part 2: {calibration_results(ops_2)}")
