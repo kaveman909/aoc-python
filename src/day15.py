136abf835f29f68ffae5fd5adb6d03e078c12547
David Tracy <david.tracy3@gmail.com>
2024-12-15 11:02:03 -0700

    day 15 pt 1

--- src/day15.py
+++ src/day15.py
@@ -1,12 +1,46 @@
 #!/opt/homebrew/bin/python3
 
 import sys
-import numpy as np
+from numpy import array, where, ndindex
+from collections import deque
 
 lines = open(sys.argv[1]).readlines()
 split = lines.index("\n")
-grid = np.array([[c for c in line.strip()] for line in lines[0:split]])
-instructions = "".join([line.strip() for line in lines[split + 1:]])
+grid = array([[c for c in line.strip()] for line in lines[0:split]])
+movements = "".join([line.strip() for line in lines[split + 1:]])
 
-print(grid)
-print(instructions)
+rows, cols = grid.shape
+
+bot = array([int(i[0]) for i in where(grid == '@')])
+
+direction_map = {'<': array((0, -1)),
+                 'v': array((1, 0)),
+                 '>': array((0, 1)),
+                 '^': array((-1, 0))}
+
+for move in movements:
+  # look in direction of movement until we hit
+  # either a space or a wall
+  view = direction_map[move]
+  stack = deque()
+  next_space = tuple(view + bot)
+  while grid[next_space] != "." and grid[next_space] != "#":
+    stack.append(grid[next_space])
+    next_space = tuple(array(next_space) + view)
+  if grid[next_space] == "#":
+    continue
+  else:
+    while not len(stack) == 0:
+      grid[next_space] = stack.pop()
+      next_space = tuple(array(next_space) - view)
+    # at this point back to our original "view" (one step)
+    grid[tuple(bot)] = "."
+    bot += view
+    grid[tuple(bot)] = "@"
+
+total = 0
+for i in ndindex(grid.shape):
+  if grid[i] == "O":
+    total += i[0]*100 + i[1]
+
+print(f"Part 1: {total}")
