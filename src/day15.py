dbb83ce0a424e18a2a0d35e09e7479b649a154d7
David Tracy <david.tracy3@gmail.com>
2024-12-15 17:43:16 -0700

    create new pt2 grid

--- src/day15.py
+++ src/day15.py
@@ -3,20 +3,37 @@
 import sys
 from numpy import array, where, ndindex
 from collections import deque
+from typing import List
 
 lines = open(sys.argv[1]).readlines()
 split = lines.index("\n")
-grid = array([[c for c in line.strip()] for line in lines[0:split]])
+grid = [[c for c in line.strip()] for line in lines[0:split]]
+grid2: List[List[str]] = []
+for row in range(len(grid)):
+  grid2.append([])
+  for col in range(len(grid[0])):
+    cell = grid[row][col]
+    if cell == "O":
+      grid2[-1].append("[")
+      grid2[-1].append("]")
+    elif cell == "@":
+      grid2[-1].append(cell)
+      grid2[-1].append(".")
+    else:
+      grid2[-1].append(cell)
+      grid2[-1].append(cell)
+
+grid = array(grid)
+grid2 = array(grid2)
+
 movements = "".join([line.strip() for line in lines[split + 1:]])
-
-rows, cols = grid.shape
-
-bot = array([int(i[0]) for i in where(grid == '@')])
-
 direction_map = {'<': array((0, -1)),
                  'v': array((1, 0)),
                  '>': array((0, 1)),
                  '^': array((-1, 0))}
+
+bot = array([int(i[0]) for i in where(grid == '@')])
+
 
 for move in movements:
   # look in direction of movement until we hit
@@ -44,3 +61,10 @@ for i in ndindex(grid.shape):
     total += i[0]*100 + i[1]
 
 print(f"Part 1: {total}")
+
+#################################################
+
+bot = array([int(i[0]) for i in where(grid2 == '@')])
+
+for movement in movements:
+  pass
