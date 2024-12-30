0ac6d79c614c3491c32c37d3061edc2c55a49179
David Tracy <david.tracy3@gmail.com>
2024-12-30 15:16:39 -0700

    improve reduce

--- src/day18.py
+++ src/day18.py
@@ -3,8 +3,4 @@ from functools import reduce
 
 coords = [(int(ll[0]), int(ll[1])) for l in open(sys.argv[1]).readlines() if (ll := l.strip().split(","))]
 
-print(coords)
-max_x = reduce(lambda x, y: max(x,y[0]), coords, 0)
-max_y = reduce(lambda x, y: max(x,y[1]), coords, 0)
-
-
+mx, my = reduce(lambda x, y: (max(x[0], y[0]), max(x[1] ,y[1])), coords, (0, 0))
