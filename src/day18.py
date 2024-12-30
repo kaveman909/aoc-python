2628b2580a8d8ab379e72c49d8c70ab32eaa3e21
David Tracy <david.tracy3@gmail.com>
2024-12-30 15:28:29 -0700

    grid populated with corruption

--- src/day18.py
+++ src/day18.py
@@ -1,6 +1,12 @@
 import sys
 from functools import reduce
+from numpy import array
 
-coords = [(int(ll[0]), int(ll[1])) for l in open(sys.argv[1]).readlines() if (ll := l.strip().split(","))]
+coords = [(int(ll[1]), int(ll[0])) for l in open(sys.argv[1]).readlines() if (ll := l.strip().split(","))]
 
-mx, my = reduce(lambda x, y: (max(x[0], y[0]), max(x[1] ,y[1])), coords, (0, 0))
+my, mx = reduce(lambda x, y: (max(x[0], y[0]), max(x[1] ,y[1])), coords, (0, 0))
+
+grid = array([["."] * (mx + 1) for _ in range((my + 1))])
+
+for i in range(12):
+  grid[coords[i]] = "#"
