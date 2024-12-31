33123298808cae700aad34e5bf2a2f9f6d24c2d6
David Tracy <david.tracy3@gmail.com>
2024-12-30 22:32:59 -0700

    day 19 pt 2 only works on example, need some sort of caching

--- src/day19.py
+++ src/day19.py
@@ -8,14 +8,14 @@ designs = [l.strip() for l in lines[2:]]
 
 
 def check_pattern(design, offset, max_length):
+  total = 0
   for length in range(offset, max_length):
     sub_pattern = design[offset:length + 1]
     if sub_pattern in patterns:
       if len(sub_pattern) + offset == max_length:
-        return True
-      if check_pattern(design, length + 1, max_length) is True:
-        return True
-  return False
+        total += 1
+      total += check_pattern(design, length + 1, max_length)
+  return total
 
 
 print(f"Part 1: {reduce(lambda x, y: x +
