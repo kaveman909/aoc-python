60dcff3d41ce9221ed84c1a3dd3ae1c71e8b530d
David Tracy <david.tracy3@gmail.com>
2024-12-30 23:26:07 -0700

    replace manual cache with builtin @cache, just learned of this

--- src/day19.py
+++ src/day19.py
@@ -1,15 +1,13 @@
 import sys
-from typing import Dict
+from functools import cache
 
 lines = open(sys.argv[1]).readlines()
 
 patterns = lines[0].strip().split(", ")
 designs = [l.strip() for l in lines[2:]]
 
-
-def check_pattern(design, offset, max_length, cache: Dict[int, int]):
-  if offset in cache:
-    return cache[offset]
+@cache
+def check_pattern(design, offset, max_length):
   total = 0
   for length in range(offset, max_length):
     sub_pattern = design[offset:length + 1]
@@ -17,19 +15,11 @@ def check_pattern(design, offset, max_length, cache: Dict[int, int]):
       if len(sub_pattern) + offset == max_length:
         total += 1
       else:
-        total += check_pattern(design, length + 1, max_length, cache)
-  # Only process an offset once
-  cache[offset] = total
+        total += check_pattern(design, length + 1, max_length)
   return total
 
 
-total = 0
-total2 = 0
-for design in designs:
-  cache = {}
-  ret = check_pattern(design, 0, len(design), cache)
-  total += ret > 0
-  total2 += ret
+counts = [check_pattern(design, 0, len(design)) for design in designs]
 
-print(f"Part 1: {total}")
-print(f"Part 2: {total2}")
+print(f"Part 1: {sum(map(bool, counts))}")
+print(f"Part 2: {sum(counts)}")
