25142b6efabb5ed9b70e7a7fbc63bbd45fbe5b6d
David Tracy <david.tracy3@gmail.com>
2024-12-31 15:16:28 -0700

    populate base graph, make sure dist is 84 for example input

--- src/day20.py
+++ src/day20.py
@@ -1,5 +1,6 @@
 import sys
 from numpy import array, where
+from networkx import Graph, shortest_path_length
 
 grid = array([[c for c in l.strip()] for l in open(sys.argv[1]).readlines()])
 rows, cols = grid.shape
@@ -10,6 +11,24 @@ s = tuple([int(i[0]) for i in where(grid == "S")])
 e = tuple([int(i[0]) for i in where(grid == "E")])
 
 wtrack = where(grid == ".")
-track = [(int(y), int(x)) for y, x in zip(wtrack[0], wtrack[1])]
+tracks = [(int(y), int(x)) for y, x in zip(wtrack[0], wtrack[1])]
+tracks.append(s)
+tracks.append(e)
+print(tracks)
 
-print(track)
+dirs = [(0, 1), (0, -1),
+        (1, 0), (-1, 0)]
+
+adj_dict = {}
+
+for track in tracks:
+  adj_list = []
+  for dir in dirs:
+    nbr = tuple(array(track) + array(dir))
+    if nbr in tracks:
+      ny, nx = nbr
+      adj_list.append((int(ny), int(nx)))
+  adj_dict[track] = tuple(adj_list)
+
+g = Graph(adj_dict)
+print(shortest_path_length(g, s, e))
