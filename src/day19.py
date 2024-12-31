8a9196d6d2dc005631cc84a8d324ae9e5da15873
David Tracy <david.tracy3@gmail.com>
2024-12-30 22:24:26 -0700

    day 19 pt 1

--- src/day19.py
+++ src/day19.py
@@ -1,9 +1,22 @@
 import sys
+from functools import reduce
 
 lines = open(sys.argv[1]).readlines()
 
 patterns = lines[0].strip().split(", ")
-print(patterns)
-
 designs = [l.strip() for l in lines[2:]]
-print(designs)
+
+
+def check_pattern(design, offset, max_length):
+  for length in range(offset, max_length):
+    sub_pattern = design[offset:length + 1]
+    if sub_pattern in patterns:
+      if len(sub_pattern) + offset == max_length:
+        return True
+      if check_pattern(design, length + 1, max_length) is True:
+        return True
+  return False
+
+
+print(f"Part 1: {reduce(lambda x, y: x +
+      check_pattern(y, 0, len(y)), designs, 0)}")
