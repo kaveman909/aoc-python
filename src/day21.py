a89bc7efab1a8200e771e8b9184256bdb7765447
David Tracy <david.tracy3@gmail.com>
2025-01-01 00:53:02 -0700

    added graph for dir keypad. tested shorted paths works on both keypad types

--- src/day21.py
+++ src/day21.py
@@ -1,7 +1,7 @@
 import sys
 from networkx import DiGraph, all_shortest_paths
 
-codes = [[int(c, 16) for c in l.strip()]
+codes = [[c for c in l.strip()]
          for l in open(sys.argv[1]).readlines()]
 
 
@@ -30,19 +30,20 @@ def get_shortest_path_dirs(g, src, dst):
 #     | 0 | A |
 #     +---+---+
 adj_dict_numeric = {
-    7: {8: {'d': '>'}, 4: {'d': 'v'}},
-    8: {7: {'d': '<'}, 5: {'d': 'v'}, 9: {'d': '>'}},
-    9: {8: {'d': '<'}, 6: {'d': 'v'}},
-    4: {7: {'d': '^'}, 5: {'d': '>'}, 1: {'d': 'v'}},
-    5: {4: {'d': '<'}, 8: {'d': '^'}, 6: {'d': '>'}, 2: {'d': 'v'}},
-    1: {4: {'d': '^'}, 2: {'d': '>'}},
-    2: {1: {'d': '<'}, 5: {'d': '^'}, 3: {'d': '>'}, 0: {'d': 'v'}},
-    3: {6: {'d': '^'}, 2: {'d': '<'}, 10: {'d': 'v'}},
-    0: {2: {'d': '^'}, 10: {'d': '>'}},
-    10: {0: {'d': '<'}, 3: {'d': '^'}}
+    '7': {'8': {'d': '>'}, '4': {'d': 'v'}},
+    '8': {'7': {'d': '<'}, '5': {'d': 'v'}, '9': {'d': '>'}},
+    '9': {'8': {'d': '<'}, '6': {'d': 'v'}},
+    '4': {'7': {'d': '^'}, '5': {'d': '>'}, '1': {'d': 'v'}},
+    '5': {'4': {'d': '<'}, '8': {'d': '^'}, '6': {'d': '>'}, '2': {'d': 'v'}},
+    '1': {'4': {'d': '^'}, '2': {'d': '>'}},
+    '2': {'1': {'d': '<'}, '5': {'d': '^'}, '3': {'d': '>'}, '0': {'d': 'v'}},
+    '3': {'6': {'d': '^'}, '2': {'d': '<'}, 'A': {'d': 'v'}},
+    '0': {'2': {'d': '^'}, 'A': {'d': '>'}},
+    'A': {'0': {'d': '<'}, '3': {'d': '^'}}
 }
 graph_numeric = DiGraph(adj_dict_numeric)
-print(get_shortest_path_dirs(graph_numeric, 7, 10))
+print(get_shortest_path_dirs(graph_numeric, '7', 'A'))
+print(get_shortest_path_dirs(graph_numeric, 'A', '7'))
 
 # Directional keypad
 #     +---+---+
@@ -51,5 +52,12 @@ print(get_shortest_path_dirs(graph_numeric, 7, 10))
 # | < | v | > |
 # +---+---+---+
 adj_dict_dir = {
-
+    '^': {'A': {'d': '>'}, 'v': {'d': 'v'}},
+    'A': {'^': {'d': '<'}, '>': {'d': 'v'}},
+    '<': {'v': {'d': '>'}},
+    'v': {'<': {'d': '<'}, '^': {'d': '^'}, '>': {'d': '>'}},
+    '>': {'v': {'d': '<'}, 'A': {'d': '^'}}
 }
+graph_dir = DiGraph(adj_dict_dir)
+print(get_shortest_path_dirs(graph_dir, 'A', '<'))
+print(get_shortest_path_dirs(graph_dir, '<', 'A'))
